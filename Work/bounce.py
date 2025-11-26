2dd80c4fe155649d1825373e0c49ee7d8f754795
xigeras <jbharris62@gmail.com>
2025-11-25 22:41:31 -0600

    bounce.py finished

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
-#
+# A rubber ball is dropped from a height of 100 meters and each  time it hits the ground, it bounches back up to 3/5 the height it fell.
 # Exercise 1.5
+
+height = 100
+bounce = 1
+
+
+
+while bounce <= 10:
+    height = height * (3/5)
+    print(bounce, round(height, 4))
+    bounce += 1
