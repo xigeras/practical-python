dc78cd5f3ddaa85b8642f4fba3a472004dd56b2a
xigeras <jbharris62@gmail.com>
2025-11-25 23:55:04 -0600

    mmmmmmmmmmmmmmmmmmmmmmmmmmm yum

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,31 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+# Dave has decided to take out a 30-year fixed rate mortgage of $500,000 with Guido’s Mortgage, Stock Investment, and Bitcoin trading corporation. The interest rate is 5% and the monthly payment is $2684.11.
+
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+extra_payment = 1000.0
+extra_payment_start_month = 61  
+extra_payment_end_month = 108
+
+while principal > 0:
+    month = month + 1
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+
+    print(month, round(total_paid,2), round(principal,2))
+
+print('Total paid', round(total_paid,2))
+print('Months', month)
+
--- Work/sears.py
+++ Work/sears.py
@@ -1,12 +1,9 @@
 # One morning, you go out and place a dollar bill on the sidewalk by the Sears  tower in Chicago. Each day thereafter, you go out double the number of bills. How long does it take for the stack of bills to exceed the height of the tower?
 
-
-
-bill_thickness = 0.11 * 0.001 # meters (0.11 mm)
-sears_height = 442 # height (meters)
-num_bills = 1
-day = 1
-
+bill_thickness = 0.11 * 0.001    # Meters (0.11 mm)
+sears_height   = 442             # Height (meters)
+num_bills      = 1
+day            = 1
 
 while num_bills * bill_thickness < sears_height:
     print(day, num_bills, num_bills * bill_thickness)
